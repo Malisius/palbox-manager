709d053951db2b68124ee77ef132a69819208ed5
Malisius <me@peteroertel.com>
2024-02-06 18:55:46 -0500

    Insane refactoring

--- manager/__main__.py
+++ manager/__main__.py
@@ -2,11 +2,78 @@
 # Released to the public under CC BY-NC (https://creativecommons.org/licenses/by-nc/4.0/)
 
 import json
-from flask_app.webapp import app
 import sys
 from pathlib import Path
+from flask import Flask, render_template, request, session, redirect, url_for
+from logging.config import dictConfig
+from functools import wraps
 
 CONFIG_FILEPATH: str = "config/palbox_config.json"
+config = {}
+
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "DEBUG", "handlers": ["wsgi"]},
+    }
+)
+
+app = Flask(__name__)
+
+
+def check_allowed(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if not config.get("manager"):
+            app.logger.info(
+                "Didn't find correct manager configuration, redirecting to first setup."
+            )
+            return redirect(url_for("setup"))
+
+        if not session.get("signedin"):
+            return redirect(url_for("signin"))
+
+        return func(*args, **kwargs)
+
+    return inner
+
+
+@app.route("/")
+@check_allowed
+def home() -> str:
+    return render_template("dashboard.html", location="dashboard")
+
+
+@app.route("/signin", methods=["GET", "POST"])
+@check_allowed
+def signin() -> str:
+    if request.method == "GET":
+        return render_template("signin.html")
+
+
+@app.route("/stats")
+@check_allowed
+def stats() -> str:
+    return render_template("stats.html", location="stats")
+
+
+@check_allowed
+@app.route("/setup")
+def setup() -> str:
+    return render_template("setup.html", location="setup")
 
 
 def main(args=None):
@@ -14,7 +81,6 @@ def main(args=None):
     try:
         Path("config").mkdir(exist_ok=True)
         app.logger.debug("Reading config json file")
-        config = {}
         with open(CONFIG_FILEPATH, "r") as f:
             config = json.load(f)
     except FileNotFoundError as e:
@@ -23,7 +89,7 @@ def main(args=None):
         )
         with open(CONFIG_FILEPATH, "w") as f:
             json.dump(config, f)
-    app.logger.info(config)
+    app.logger.debug(config)
     app.run(debug=True)
 
 
